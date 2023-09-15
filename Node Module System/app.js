8df4f215a089194e7f753b00d487a2829af53eb9
Yassine Slaoui <yassine_slaoui51@outlook.fr>
2023-09-15 14:30:11 +0100

    used fs (FileSystem) part 2 module/library

--- Node Module System/app.js
+++ Node Module System/app.js
@@ -1,4 +1,13 @@
 const fsModule = require('fs');
 
-const files = fsModule.readdirSync(".");
-console.log(files);
+// const files = fsModule.readdirSync(".");
+// console.log(files);
+
+fsModule.readdir("./s", (err, files) => {
+    if (err) {
+        console.error(`Error reading directory!, ${err}`);
+    }
+    if (files) {
+        console.log(`Files are: ${files}`);
+    }
+})
