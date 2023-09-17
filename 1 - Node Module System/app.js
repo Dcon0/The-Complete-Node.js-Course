739be06daaff6ad6b18ef3a1082655725f3b8ff3
Yassine Slaoui <yassine_slaoui51@outlook.fr>
2023-09-17 14:17:49 +0100

    Using http API Part 2 Requests and Responses

--- 1 - Node Module System/app.js
+++ 1 - Node Module System/app.js
@@ -1,10 +1,17 @@
 const http = require("http");
 
 const port = 3000
-const server = http.createServer();
-server.listen(port);
-server.on("connection", (socket) => {
+const server = http.createServer((req, res) => {
     console.log("New connection!");
+    // console.log("Request:\n", req);
+    console.log("Request URL:", req.url);
+    if (req.url === "/") {
+        res.write("Hello");
+        res.end(", World!");
+    }
+    if (req.url === '/api/courses')
+        res.end(JSON.stringify([1, 2, 3, 4, 5]));
 });
+server.listen(port);
 
 console.log("Listening on port", port + "...");
