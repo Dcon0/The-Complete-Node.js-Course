ce45e2fa65a9a2b0aed454ba8c9d25edd5bcb211
Yassine Slaoui <yassine_slaoui51@outlook.fr>
2023-09-22 15:54:11 +0100

    Handling HTTP Put Requests

--- 3 - Building RESTful API's Using Express/express-demo/index.js
+++ 3 - Building RESTful API's Using Express/express-demo/index.js
@@ -4,7 +4,11 @@ const app = express();
 
 app.use(express.json());
 
-const courses = [{ id: 1, name: 'Course One' },
+const schema = Joi.object({
+    name: Joi.string().min(3).required()
+})
+
+let courses = [{ id: 1, name: 'Course One' },
 { id: 2, name: 'Course Two' }];
 
 app.get('/', (req, res) => {
@@ -15,17 +19,17 @@ app.get('/api/courses', (req, res) => {
     res.send(courses);
 })
 
-app.post('/api/courses', (req, res) => {
-    const schema = Joi.object({
-        name: Joi.string().min(3).required()
-    })
+app.get('/api/courses/:id', (req, res) => {
+    const course = courses.find(item => item.id === parseInt(req.params.id));
+    //404 - Resource Not Found
+    if (!course) res.status(404).send("The requested course doesn't exist!");
+    else res.send(course);
+})
 
+app.post('/api/courses', (req, res) => {
     const result = schema.validate(req.body);
 
-    if (result.error) {
-        res.status(400).send(result.error.details[0].message);
-        return;
-    }
+    if (result.error) { res.status(400).send(result.error.details[0].message); return; }
 
     const course = {
         id: courses.length + 1,
@@ -35,13 +39,25 @@ app.post('/api/courses', (req, res) => {
     res.send(course);
 })
 
-app.get('/api/courses/:id', (req, res) => {
-    const course = courses.find((item) => {
-        return item.id == req.params.id;
-    })
-    //404 - Resource Not Exist
-    if (!course) res.status(404).send("The requested course doesn't exist!");
-    else res.send(course);
+app.put('/api/courses/:id', (req, res) => {
+    const requestedId = parseInt(req.params.id)
+
+    const course = courses.find(item => item.id === requestedId);
+    if (!course) { res.status(404).send("The requested course doesn't exist!"); return; }
+
+    const { error } = schema.validate(req.body);
+    if (error) { res.status(400).send(error.details[0].message); return; }
+
+    // courses = courses.map((item) => {
+    //     if (item.id === requestedId) {
+    //         item.name = req.body.name;
+    //     }
+    //     return item;
+    // })
+
+    course.name = req.body.name;
+
+    res.send(course);
 })
 
 const port = process.env.PORT || 3000;
