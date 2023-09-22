f806cd927399b48ba7a35504d0168335e94eb5e7
Yassine Slaoui <yassine_slaoui51@outlook.fr>
2023-09-22 15:08:17 +0100

    Handling HTTP POST Requests

--- 3 - Building RESTful API's Using Express/express-demo/index.js
+++ 3 - Building RESTful API's Using Express/express-demo/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 const courses = [{ id: 1, name: 'Course One' },
 { id: 2, name: 'Course Two' }];
 
@@ -12,13 +14,21 @@ app.get('/api/courses', (req, res) => {
     res.send(courses);
 })
 
+app.post('/api/courses', (req, res) => {
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+    courses.push(course);
+    res.send(course);
+})
+
 app.get('/api/courses/:id', (req, res) => {
     const course = courses.find((item) => {
         return item.id == req.params.id;
     })
     if (!course) res.status(404).send("The requested course doesn't exist!");
-    else res.send(`You requested course id ${req.params.id}:
-    ${JSON.stringify(course)}`);
+    else res.send(course);
 })
 
 const port = process.env.PORT || 3000;
