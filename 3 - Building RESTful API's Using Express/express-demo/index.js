f73d43d2ba9216767a286b64ce87ce0571627c08
Yassine Slaoui <yassine_slaoui51@outlook.fr>
2023-09-23 12:11:10 +0100

    Middleware

--- 3 - Building RESTful API's Using Express/express-demo/index.js
+++ 3 - Building RESTful API's Using Express/express-demo/index.js
@@ -1,8 +1,14 @@
 const Joi = require('joi');
 const express = require('express');
+const log = require('./logger');
+const auth = require('./auth');
+
 const app = express();
 
 app.use(express.json());
+app.use(log);
+app.use(auth);
+
 
 const schema = Joi.object({
     name: Joi.string().min(3).required()
