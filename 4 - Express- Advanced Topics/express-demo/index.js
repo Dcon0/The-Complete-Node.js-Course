e33c5b9e114209dcc272e52667a879e4296e04d1
Yassine Slaoui <yassine_slaoui51@outlook.fr>
2023-09-25 17:14:10 +0100

    Cleaned/Restructured the project

--- 4 - Express- Advanced Topics/express-demo/index.js
+++ 4 - Express- Advanced Topics/express-demo/index.js
@@ -1,93 +1,33 @@
 const startupDebugger = require('debug')('app:startup');
 const config = require('config');
-const Joi = require('joi');
 const express = require('express');
-const log = require('./logger');
+const logger = require('./middleware/logger');
 const helmet = require('helmet');
 const morgan = require('morgan');
+const courses = require('./routes/courses');
+const home = require('./routes/home');
 
 const app = express();
 
 app.set('view engine', 'pug');
 app.set('views', './views');
 
-app.use(log);
+app.use(logger);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'))
 app.use(helmet());
-
-console.log('Application Name: ' + config.get('name'));
-console.log('Mail: ' + config.get('mail.host'));
-console.log('Password: ' + config.get('mail.password'));
-
-
 if (app.get('env') === 'dev') {
     app.use(morgan('tiny'));
     startupDebugger('Morgan enabled...');
 }
-const schema = Joi.object({
-    name: Joi.string().min(3).required()
-})
 
-let courses = [{ id: 1, name: 'Course One' },
-{ id: 2, name: 'Course Two' }];
+app.use(home); //same as "app.use('/', home);"
+app.use('/api/courses', courses);
 
-app.get('/', (req, res) => {
-    res.render('index', {
-        title: "Express Demo App",
-        message: 'Hello'
-    });
-});
-
-app.get('/api/courses', (req, res) => {
-    res.send(courses);
-})
-
-app.get('/api/courses/:id', (req, res) => {
-    const course = courses.find(item => item.id === parseInt(req.params.id));
-    //404 - Resource Not Found
-    if (!course) res.status(404).send("The requested course doesn't exist!");
-    else res.send(course);
-})
-
-app.post('/api/courses', (req, res) => {
-    const result = schema.validate(req.body);
-
-    if (result.error) { res.status(400).send(result.error.details[0].message); return; }
-
-    const course = {
-        id: courses.length + 1,
-        name: req.body.name
-    };
-    courses.push(course);
-    res.send(course);
-})
-
-app.put('/api/courses/:id', (req, res) => {
-    const requestedId = parseInt(req.params.id)
-
-    const course = courses.find(item => item.id === requestedId);
-    if (!course) { res.status(404).send("The requested course doesn't exist!"); return; }
-
-    const { error } = schema.validate(req.body);
-    if (error) { res.status(400).send(error.details[0].message); return; }
-
-    course.name = req.body.name;
-
-    res.send(course);
-})
-
-app.delete('/api/courses/:id', (req, res) => {
-    const requestedId = parseInt(req.params.id)
-
-    const course = courses.find(item => item.id === requestedId);
-    if (!course) { res.status(404).send("The requested course doesn't exist!"); return; }
-
-    courses.splice(courses.indexOf(course), 1);
-
-    res.send(course);
-})
+console.log('Application Name: ' + config.get('name'));
+console.log('Mail: ' + config.get('mail.host'));
+console.log('Password: ' + config.get('mail.password'));
 
 const port = process.env.PORT || 3000;
 app.listen(port, "127.0.0.1", () => console.log(`Listening on port ${port}...`));
