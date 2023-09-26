3339e865f9abf0a777d1c2e5325e7151a9dbf380
Yassine Slaoui <yassine_slaoui51@outlook.fr>
2023-09-26 19:51:22 +0100

    Exploring the Promise-API

--- 5- Asynchronous JavaScript/async-demo/index.js
+++ 5- Asynchronous JavaScript/async-demo/index.js
@@ -1,35 +1,7 @@
-console.log('[main @' + new Date().toLocaleTimeString() + '] before');
+const resolvedPromise = Promise.resolve("Resolved :)");
+const rejectedPromise = Promise.reject(new Error('Rejected :('));
 
-getUserPromiseFunction(1)
-    .then(user => {
-        console.log('[main @' + new Date().toLocaleTimeString() + '] User:', user);
-        return getGitHubRepoPromiseFunction(user.id);
-    })
-    .then(repos => console.log('[main @' + new Date().toLocaleTimeString() + '] Repos:', repos))
-    .catch(err => console.log(err.message));
-
-console.log('[main @' + new Date().toLocaleTimeString() + '] after');
-
-function getUserPromiseFunction(id) {
-    console.log('[getUser Promise Function @' + new Date().toLocaleTimeString() + '] Fetching user');
-    return new Promise((resolve, reject) => {
-        console.log('[getUser Promise @' + new Date().toLocaleTimeString() + '] Fetching user ');
-        setTimeout(() => {
-            console.log('[getUser Promise @' + new Date().toLocaleTimeString() + '] User Found!');
-            resolve({ id: id, name: 'User with id ' + id });
-            reject(new Error('There has been an error'));
-        }, 2000);
-    });
-}
-
-function getGitHubRepoPromiseFunction(userId) {
-    console.log('[getGitHubRepo Promise Function @' + new Date().toLocaleTimeString() + '] Calling GitHub API');
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            console.log('[getGitHubRepo Promise @' + new Date().toLocaleTimeString() + '] Calling GitHub API');
-            console.log('[getGitHubRepo Promise @' + new Date().toLocaleTimeString() + '] Repo Contents of user with id ' + userId + ' Retrieved Successfully');
-            resolve(['Repo1', 'Repo2', 'Repo3']);
-            reject(new Error('There has been an error'));
-        }, 2000);
-    });
-}
+resolvedPromise.then(result => console.log(result))
+    .catch(error => console.error(error));
+rejectedPromise.then(result => console.log(result))
+    .catch(error => console.error(error));
