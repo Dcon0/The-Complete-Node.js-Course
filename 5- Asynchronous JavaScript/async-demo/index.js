2f8693405e98b1be724394f7523cc3c678d543c7
Yassine Slaoui <yassine_slaoui51@outlook.fr>
2023-09-26 14:14:48 +0100

    Asynchronous JS using Promises

--- 5- Asynchronous JavaScript/async-demo/index.js
+++ 5- Asynchronous JavaScript/async-demo/index.js
@@ -1,36 +1,34 @@
 console.log('[main @' + new Date().toLocaleTimeString() + '] before');
-getUser(1, showUserAndReposCallback);
+
+getUserPromiseFunction(1).then(result => {
+    console.log('[main @' + new Date().toLocaleTimeString() + '] User:', result);
+    getGitHubRepoPromiseFunction(result.id).then(result => console.log('[main @' + new Date().toLocaleTimeString() + '] Repos:', result))
+        .catch(err => console.log(err.message));
+})
+    .catch(err => console.log(err.message));
+
 console.log('[main @' + new Date().toLocaleTimeString() + '] after');
 
-function getUser(id, callback) {
-    console.log('[getUser Function @' + new Date().toLocaleTimeString() + '] Fetching user');
-    setTimeout(() => {
-        console.log('[getUser Function @' + new Date().toLocaleTimeString() + '] User Found!');
-        callback({ id: id, name: 'User with id ' + id }, null);
-    }, 2000)
+function getUserPromiseFunction(id) {
+    console.log('[getUser Promise Function @' + new Date().toLocaleTimeString() + '] Fetching user');
+    return new Promise((resolve, reject) => {
+        console.log('[getUser Promise @' + new Date().toLocaleTimeString() + '] Fetching user ');
+        setTimeout(() => {
+            console.log('[getUser Promise @' + new Date().toLocaleTimeString() + '] User Found!');
+            resolve({ id: id, name: 'User with id ' + id });
+            reject(new Error('There has been an error'));
+        }, 2000);
+    });
 }
 
-function getGitHubRepo(userId, callback) {
-    console.log('[getGitHubRepo Function @' + new Date().toLocaleTimeString() + '] Calling GitHub API');
-    setTimeout(() => {
-        console.log('[getGitHubRepo Function @' + new Date().toLocaleTimeString() + '] Repo Contents Retrieved Successfully');
-        callback(['Repo1', 'Repo2', 'Repo3'], null);
-    }, 2000);
-}
-
-function showUserReposCallback(repos, error) {
-    if (repos) {
-        console.log('[showUserReposCallback Function @' + new Date().toLocaleTimeString() + '] Repos:', repos);
-    }
-    else
-        console.log('There was an error!');
-}
-
-function showUserAndReposCallback(user, err) {
-    if (user) {
-        console.log('[showUserAndReposCallback @' + new Date().toLocaleTimeString() + '] User:', user);
-        getGitHubRepo(user.id, showUserReposCallback);
-    }
-    else
-        console.log('There was an error!');
+function getGitHubRepoPromiseFunction(userId) {
+    console.log('[getGitHubRepo Promise Function @' + new Date().toLocaleTimeString() + '] Calling GitHub API');
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('[getGitHubRepo Promise @' + new Date().toLocaleTimeString() + '] Calling GitHub API');
+            console.log('[getGitHubRepo Promise @' + new Date().toLocaleTimeString() + '] Repo Contents Retrieved Successfully');
+            resolve(['Repo1', 'Repo2', 'Repo3']);
+            reject(new Error('There has been an error'));
+        }, 2000);
+    });
 }
