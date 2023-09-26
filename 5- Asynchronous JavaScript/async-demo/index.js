552ac459a5500488c3af63f9d2ed114d31928140
Yassine Slaoui <yassine_slaoui51@outlook.fr>
2023-09-26 20:26:32 +0100

    More Promise API exploration

--- 5- Asynchronous JavaScript/async-demo/index.js
+++ 5- Asynchronous JavaScript/async-demo/index.js
@@ -1,7 +1,30 @@
-const resolvedPromise = Promise.resolve("Resolved :)");
-const rejectedPromise = Promise.reject(new Error('Rejected :('));
+const promiseOne = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve({ promiseId: 1, name: "Promise One" });
+    }, 1000);
+})
 
-resolvedPromise.then(result => console.log(result))
-    .catch(error => console.error(error));
-rejectedPromise.then(result => console.log(result))
-    .catch(error => console.error(error));
+const promiseTwo = new Promise((resolve) => {
+    setTimeout(() => {
+        resolve({ promiseId: 2, name: "Promise Two" });
+    }, 2000);
+})
+
+const promiseThree = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        // resolve({ promiseId: 3, name: "Promise Three" });
+        reject(new Error("This is not on purpose hehe.."));
+    }, 2000);
+})
+
+Promise.all([promiseOne, promiseTwo]).then(result => console.log('[Promise.all([promiseOne, promiseTwo]).then]', result))
+    .catch(error => console.error('[Promise.all([promiseOne, promiseTwo]).catch]', error));
+
+Promise.all([promiseOne, promiseTwo, promiseThree]).then(result => console.log('[Promise.all([promiseOne, promiseTwo, promiseThree]).then]', result))
+    .catch(error => console.error('[Promise.all([promiseOne, promiseTwo, promiseThree]).catch]', error));
+
+Promise.race([promiseOne, promiseTwo, promiseThree]).then(result => console.log('[Promise.race([promiseOne, promiseTwo, promiseThree]).then]', result))
+    .catch(error => console.error('[Promise.race([promiseOne, promiseTwo, promiseThree]).catch]', error));
+
+Promise.allSettled([promiseOne, promiseTwo, promiseThree]).then(result => console.log('[Promise.allSettled([promiseOne, promiseTwo, promiseThree]).then]', result))
+    .catch(error => console.error('[Promise.allSettled([promiseOne, promiseTwo, promiseThree]).catch]', error));
