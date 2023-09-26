af6ba73d6526af2770f6428885a3ddeaa37fcfe1
Yassine Slaoui <yassine_slaoui51@outlook.fr>
2023-09-26 12:03:03 +0100

    Named Callback Functions instead

--- 5- Asynchronous JavaScript/async-demo/index.js
+++ 5- Asynchronous JavaScript/async-demo/index.js
@@ -1,16 +1,5 @@
 console.log('[main @' + new Date().toLocaleTimeString() + '] before');
-
-getUser(1, (user, err) => {
-    if (user) {
-        console.log('[main @' + new Date().toLocaleTimeString() + '] User:', user);
-        getGitHubRepo(user.id, (repo, error) => {
-            console.log('[main @' + new Date().toLocaleTimeString() + '] User: "' + user.name + '" has these repos:', repo);
-        })
-    }
-    else
-        console.log('There was an error!');
-})
-
+getUser(1, showUserAndReposCallback);
 console.log('[main @' + new Date().toLocaleTimeString() + '] after');
 
 function getUser(id, callback) {
@@ -27,4 +16,21 @@ function getGitHubRepo(userId, callback) {
         console.log('[getGitHubRepo Function @' + new Date().toLocaleTimeString() + '] Repo Contents Retrieved Successfully');
         callback(['Repo1', 'Repo2', 'Repo3'], null);
     }, 2000);
+}
+
+function showUserReposCallback(repos, error) {
+    if (repos) {
+        console.log('[showUserReposCallback Function @' + new Date().toLocaleTimeString() + '] Repos:', repos);
+    }
+    else
+        console.log('There was an error!');
+}
+
+function showUserAndReposCallback(user, err) {
+    if (user) {
+        console.log('[showUserAndReposCallback @' + new Date().toLocaleTimeString() + '] User:', user);
+        getGitHubRepo(user.id, showUserReposCallback);
+    }
+    else
+        console.log('There was an error!');
 }
