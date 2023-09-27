814399a1d38a4789cabf5238e05dd4222f880dc9
Yassine Slaoui <yassine_slaoui51@outlook.fr>
2023-09-27 21:08:27 +0100

    Async and Await V1

--- 5- Asynchronous JavaScript/async-demo/index.js
+++ 5- Asynchronous JavaScript/async-demo/index.js
@@ -1,11 +1,19 @@
 console.log('[main @' + new Date().toLocaleTimeString() + '] before');
 
-getUserPromiseFunction(1)
-    .then(user => getGitHubRepoPromiseFunction(user.id))
-    .then(repos => console.log('[main @' + new Date().toLocaleTimeString() + '] Repos:', repos))
-    .catch(err => console.log(err.message))
+getGitHubReposAsync(1);
 
 console.log('[main @' + new Date().toLocaleTimeString() + '] after');
+
+async function getGitHubReposAsync(userId) {
+    try {
+        const user = await getUserPromiseFunction(1);
+        const repos = await getGitHubReposPromiseFunction(user.id);
+        console.log('[getGitHubReposAsync Async Function @' + new Date().toLocaleTimeString() + '] User:', user);
+        console.log('[getGitHubReposAsync Async Function @' + new Date().toLocaleTimeString() + '] Repos:', repos);
+    } catch (error) {
+        console.error(error);
+    }
+}
 
 function getUserPromiseFunction(id) {
     console.log('[getUser Promise Function @' + new Date().toLocaleTimeString() + '] Fetching user');
@@ -14,19 +22,19 @@ function getUserPromiseFunction(id) {
         setTimeout(() => {
             console.log('[getUser Promise @' + new Date().toLocaleTimeString() + '] User Found!');
             resolve({ id: id, name: 'User with id ' + id });
-            reject(new Error('There has been an error'));
+            reject(new Error('There has been an error retrieving user!'));
         }, 2000);
     });
 }
 
-function getGitHubRepoPromiseFunction(userId) {
+function getGitHubReposPromiseFunction(userId) {
     console.log('[getGitHubRepo Promise Function @' + new Date().toLocaleTimeString() + '] Calling GitHub API');
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             console.log('[getGitHubRepo Promise @' + new Date().toLocaleTimeString() + '] Calling GitHub API');
             console.log('[getGitHubRepo Promise @' + new Date().toLocaleTimeString() + '] Repo Contents of user with id ' + userId + ' Retrieved Successfully');
-            resolve(['Repo1', 'Repo2', 'Repo3']);
-            reject(new Error('There has been an error'));
+            // resolve(['Repo1', 'Repo2', 'Repo3']);
+            reject(new Error('There has been an error retrieving repos!'));
         }, 2000);
     });
 }
